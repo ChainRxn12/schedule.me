145a95ccc735c863d17ad4161d2db8f4b14be605
Billy Dorman <b.d.dorman12@gmail.com>
2021-05-24 22:40:12 -0500

    pseudocoding time change events in script.js, prepping css and html to receive events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 //assign variables
 var timeDisplay = $('#time-display'); //get time display to work in jumbotron
+var scheduleInput = $('#schedule-input');//create text/schedule input for user
 
-
-
-
+var currentTime;
+var scheduledTime;
 
 
 //display time function in jumbotron
@@ -12,3 +12,22 @@ function displayTime() {
     timeDisplay.text(rightNow);
 }
 setInterval(displayTime, 1000);
+
+            //pseudocode//
+
+// if currentTime > scheduledTime
+// then what? past class needs to be activated
+//             no future class
+//             no present class
+
+// if currentTime < scheduledTime
+// then what? future class needs to be activated
+//             no past class
+//             no present class
+
+// if currentTime === scheduledTime
+// then what? present class next to be activated
+//             no future class
+//             no past class
+
+//need a function for color change colorChange()
