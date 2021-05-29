ed01203a2855555bf4c93c40c3bc6e15417e6a78
Billy Dorman <b.d.dorman12@gmail.com>
2021-05-29 11:25:58 -0500

    create each loop to time blocks according to current time, script.js updating html/css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,94 @@
 //assign variables
-var timeDisplay = $('#time-display'); //get time display to work in jumbotron
-var scheduleInputElement = $('#schedule-input');//create text/schedule input for user
+var timeDisplay = $("#time-display"); //get time display to work in jumbotron
+// var scheduleInputElement = $(".schedule-input").attr("id"); //create text/schedule input for user
+var currentHour = moment().hour();
+var scheduleInput = $(".schedule-input");
 
-var scheduledTime;
 
 //display time function in jumbotron
 function displayTime() {
-    var rightNow = moment().format('MMM DD, [at] hh:mm:ss a');
-    timeDisplay.text(rightNow);
+  var rightNow = moment().format("MMM DD, [at] hh:mm:ss a");
+  timeDisplay.text(rightNow);
 }
 setInterval(displayTime, 1000);
 
 //define rows in 24hr format
 const rows = document.getElementsByClassName("row");
-let currentTime = parseInt(moment().format('H'));
+var currentTime = parseInt(moment().format("H"));
 console.log(currentTime);
 
+//use each loop to add class to each time block according to current time
+scheduleInput.each(function () {
+    var blockHour = parseInt($(this).attr("id"));
+    console.log(blockHour);
+  
+    if (currentHour > blockHour) {
+      $(this).addClass("past");
+      //console.log($(this))
+    } else if (currentHour === blockHour) {
+      $(this).addClass("present");
+    } else if (currentHour < blockHour) {
+        $(this).addClass ("future");
+    }
+  });
+  
 
-            //pseudocode//
+//loads when page is available
+// $(document).ready(function() {
+//    // colorChange();
+//     textOutput();
+// });
 
-// if currentTime > scheduledTime
-// then what? past class needs to be activated
-//             no future class
-//             no present class
+//clipboard/savebtn functionality
+// $('.saveBtn').click(function(){
+//     scheduleText = $(this).sibling('.schedule-input').val();
+//     console.log(scheduleText);
+//     localStorage.setItem(scheduleTime, JSON.stringify(scheduleText));
 
-// if currentTime < scheduledTime
-// then what? future class needs to be activated
-//             no past class
-//             no present class
+//     textOutput();
+// });
 
-// if currentTime === scheduledTime
-// then what? present class next to be activated
-//             no future class
-//             no past class
+
+
+//pseudocode//
 
 //need a function for color change colorChange()
 
-//save the users schedule input
+
+//console.log(moment().hour());
+
+//$('.schedule-input').attr('id');
+//console.log(scheduleInputElement);
+
+
+
+// for(var i = 0; i < scheduleInput.length; i++ ){
+//     console.log(scheduleInput[i]);
+//     console.log(scheduleInput[i].id);
+//     var blockHour = scheduleInput[i].id;
+
+// // if currentTime > scheduledTime
+// // then what? past class needs to be activated
+// //             no future class
+// //             no present class
+
+// if(currentHour > blockHour){
+//     scheduleInput.addClass("past")
+//     //console.log($(this))
+// } else if ( currentHour == blockHour){
+//     scheduleInput.addClass("present")
+
+// }
+
+// // if currentTime < scheduledTime
+// // then what? future class needs to be activated
+// //             no past class
+// //             no present class
+
+// // if currentTime === scheduledTime
+// // then what? present class next to be activated
+// //             no future class
+// //             no past class
+
+// }
+
